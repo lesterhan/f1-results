c799a178df336e9edac71c08ce203489361d8363
Lester Han <lester.han@onepeloton.com>
2023-03-03 12:26:03 -0500

    tests!

--- f1_results/f1_results.py
+++ f1_results/f1_results.py
@@ -6,14 +6,21 @@ from f1_results.fixtures import SEASON_FULL_DATA_2022, PLACE_TO_POINTS_MAPPING
 def formula_1_race_results(season_data: Mapping) -> str:
     return 'We are all winners'
 
-def assign_points_for_race(race):
-    results = {}
+def assign_points_for_race(race, driver_dict):
     for i, driver in enumerate(race['result']):
-        results[driver] = get_points_for_finishing_position(i)
-    return results
+        driver_dict[driver] = get_points_for_finishing_position(i)
+    return driver_dict
 
 def get_points_for_finishing_position(finishing_position):
     return PLACE_TO_POINTS_MAPPING.get(finishing_position, 0)
+
+
+def create_driver_dict(teams):
+    dict = {}
+    for team in teams:
+        for driver in team['drivers']:
+            dict[driver['number']] = 0
+    return dict
 
 
 results = formula_1_race_results(SEASON_FULL_DATA_2022)
--- f1_results/test_f1_results.py
+++ f1_results/test_f1_results.py
@@ -3,9 +3,9 @@ from unittest import TestCase
 from f1_results.f1_results import (
     formula_1_race_results,
     assign_points_for_race,
-    get_points_for_finishing_position,
+    get_points_for_finishing_position, create_driver_dict,
 )
-from f1_results.fixtures import SEASON_FULL_DATA_2022, UNITED_STATES_2022
+from f1_results.fixtures import SEASON_FULL_DATA_2022, UNITED_STATES_2022, TEAMS_2022
 
 
 class TestF1Results(TestCase):
@@ -61,30 +61,34 @@ class TestF1Results(TestCase):
 class TestAssignPointsForRace(TestCase):
 
     def test_assign_points_for_race(self):
-       points_for_race = assign_points_for_race(UNITED_STATES_2022)
-       expected_points_for_race = {
-          1: 25,
-          44: 18,
-          16: 15,
-          11: 12,
-          63: 10,
-          4: 8,
-          14: 6,
-          5: 4,
-          20: 2,
-          22: 1,
-          31: 0,
-          24: 0,
-          23: 0,
-          10: 0,
-          47: 0,
-          3: 0,
-          6: 0,
-          18: 0,
-          77: 0,
-          55: 0,
-       }
-       self.assertEqual(points_for_race, expected_points_for_race)
+        driver_dict = create_driver_dict(TEAMS_2022)
+        points_for_race = assign_points_for_race(UNITED_STATES_2022, driver_dict)
+        expected_points_for_race = {
+            1: 25,
+            44: 18,
+            16: 15,
+            11: 12,
+            63: 10,
+            4: 8,
+            14: 6,
+            5: 4,
+            20: 2,
+            22: 1,
+            31: 0,
+            24: 0,
+            27: 0,
+            23: 0,
+            10: 0,
+            45: 0,
+            47: 0,
+            3: 0,
+            6: 0,
+            18: 0,
+            77: 0,
+            55: 0,
+        }
+        self.assertEqual(points_for_race, expected_points_for_race)
+
 
 class TestGetPointsForFinishingPosition(TestCase):
 
@@ -93,3 +97,33 @@ class TestGetPointsForFinishingPosition(TestCase):
 
     def test_get_points_for_outside_top_ten(self):
         self.assertEqual(get_points_for_finishing_position(11), 0)
+
+
+class TestCreateDriverDict(TestCase):
+    def test_create_driver_dict(self):
+        driver_dict = create_driver_dict(TEAMS_2022)
+        expected_dict = {
+            44: 0,
+            63: 0,
+            1: 0,
+            11: 0,
+            55: 0,
+            16: 0,
+            4: 0,
+            3: 0,
+            14: 0,
+            31: 0,
+            10: 0,
+            22: 0,
+            18: 0,
+            5: 0,
+            27: 0,
+            6: 0,
+            23: 0,
+            45: 0,
+            24: 0,
+            77: 0,
+            20: 0,
+            47: 0,
+        }
+        self.assertEqual(driver_dict, expected_dict)
